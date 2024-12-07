bee97bcf5e11353aa530c61160e51228779b70db
Aditya Raute <43912470+adityaraute@users.noreply.github.com>
2024-12-07 08:21:40 +0000

    Initialise setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='LatexEq',
+    version='0.0.0',
+    packages=['latexeq',],
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
