f8b57d7f946876f8a3a794353ea4ab06bf0ddeaa
Aditya Raute <2017.aditya.raute@ves.ac.in>
2024-12-11 19:04:00 +0000

    Spacing changes

--- latexeq/equation.py
+++ latexeq/equation.py
@@ -3,7 +3,6 @@ from latexSym import symbols_in_latex
 def equation(LHS, Operand, RHS):
     return f"{LHS} {Operand} {RHS}"
 
-
 def convert_symbol_to_latex(symbol_list):
     result_string = ''
     for element in symbol_list:
@@ -12,4 +11,3 @@ def convert_symbol_to_latex(symbol_list):
         except:
             print(f"{element} Character Not found")
     print(result_string)
-            
