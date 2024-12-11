13d3f25b048f56734d84546b5b5eecb8c1e82ca8
Aditya Raute <2017.aditya.raute@ves.ac.in>
2024-12-11 13:39:54 +0000

    Added MatchExpression, but might go back to JSON

--- latexeq/equation.py
+++ latexeq/equation.py
@@ -1,4 +1,31 @@
-def equation(
-LHS, Operand, RHS
-):
+def equation(LHS, Operand, RHS):
     return f"{LHS} {Operand} {RHS}"
+
+
+def convert_symbol_to_latex(symbol_list):
+    result_string = ''
+    for element in symbol_list:
+        match element:
+            case ">":
+                return ""
+            case ">=":
+                return ""
+            case "<":
+                return ""
+            case "<=":
+                return ""
+            case "=":
+                return ""
+            case "+":
+                return ""
+            case "-":
+                return ""
+            case "x":
+                return ""
+            case ".":
+                return ""
+            case "sigma":
+                return ""
+            case "integral":
+                return ""
+            
