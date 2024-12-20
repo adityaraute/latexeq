5112ad484f8c73a29cddbcbe10c23fce83efa634
Aditya Raute <2017.aditya.raute@ves.ac.in>
2024-12-20 20:11:01 +0000

    Add spaces in the output equation

--- latexeq/equation.py
+++ latexeq/equation.py
@@ -1,6 +1,5 @@
 from latexSym import symbols_in_latex
 
-
 def equation(LHS, Operand, RHS):
     return f"{LHS} {Operand} {RHS}"
 
@@ -10,8 +9,10 @@ def convert_symbol_to_latex(symbol_list):
 
     for element in symbol_list:
         if element in symbols_in_latex:
-            result_string += symbols_in_latex[element]
+            result_string += symbols_in_latex[element] + " "
         else:
             print(f"{element} Character Not found")
 
     print(result_string)
+
+convert_symbol_to_latex(["$", ">", "<=", ";", "Σ"])
